7b924e3b9d2e8f24d503fc3681619a14848c6c0b
Petrovich <yaroslav14233@gmail.com>
2026-07-15 17:53:07 +0000

    Add dependencies

---
+++ app-service/core/dependencies.py
@@ -0,0 +1,32 @@
+from core.database import AsyncSessionLocal
+from fastapi.security import OAuth2PasswordBearer
+from fastapi import Depends, HTTPException, status
+from typing import Annotated
+from sqlalchemy.ext.asyncio import AsyncSession
+import jwt
+from core.security import decode_token
+from service.auth import AuthService
+
+oauth2 = OAuth2PasswordBearer(tokenUrl="/auth/login")
+
+async def get_db():
+    with AsyncSessionLocal() as db:
+        yield db
+        
+        
+async def get_current_user(token: Annotated[str, Depends(oauth2)], db: Annotated[AsyncSession, Depends(get_db)]):
+    try: 
+        payload = decode_token(token)
+        if payload.get("type") != "access":
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid type token")
+        user_id = payload.get("sub")
+        if user_id is None:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid id user")
+    except jwt.InvalidTokenError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
+    user_service = AuthService(db)
+    user = await user_service.get_user_by_id(db, user_id)
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+    return user
+        
