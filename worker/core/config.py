46cbde0f888137313576de7ccaa3b83828387bbf
Petrovich <yaroslav14233@gmail.com>
2026-07-17 17:24:30 +0000

    Initial worker

---
+++ worker/core/config.py
@@ -0,0 +1,27 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from dotenv import find_dotenv
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=find_dotenv())
+    
+    POSTGRES_DB: str
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    DB_HOST: str = "localhost"
+    DB_PORT: int = 5432
+    RABBITMQ_DEFAULT_USER: str
+    RABBITMQ_DEFAULT_PASS: str
+    RABBITMQ_HOST: str = "localhost"
+    RABBITMQ_PORT: int = 5672
+    
+    @property
+    def database_url(self):
+        return f"postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.POSTGRES_DB}"
+    
+    @property
+    def rabbitmq_url(self):
+        return f"amqp://{self.RABBITMQ_DEFAULT_USER}:{self.RABBITMQ_DEFAULT_PASS}@{self.RABBITMQ_HOST}:{self.RABBITMQ_PORT}/"
+    
+
+settings = Settings()
