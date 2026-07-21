5e7d8b5032ff39583b3a00ef525a4ffcef595963
Petrovich <yaroslav14233@gmail.com>
2026-07-21 18:12:11 +0000

    Create tests to jobs:

--- app-service/tests/test_jobs.py
+++ app-service/tests/test_jobs.py
@@ -18,3 +18,26 @@ async def test_create_job(client: AsyncClient, auth_token: str):
     assert response.status_code == 200
     mock_send_task.assert_called_once()
     
+async def test_get_jobs(client: AsyncClient, auth_token: str):
+    with patch("service.job.celery_app.send_task") as mock_send_task:
+        response = await client.post(
+            "/jobs/",
+            json={"language": "python", "code": "print(1)"},
+            headers={"Authorization": f"Bearer {auth_token}"},
+        )
+    assert response.status_code == 200
+    mock_send_task.assert_called_once()
+    
+    jobs = await client.get(
+        "/jobs/",
+        headers={"Authorization": f"Bearer {auth_token}"})
+    
+    assert jobs.status_code == 200
+    
+async def test_create_job_not_token(client: AsyncClient):
+    with patch("service.job.celery_app.send_task") as mock_send_task:
+        response = await client.post(
+            "/jobs/",
+            json={"language": "python", "code": "print(1)"}
+        )
+    assert response.status_code == 401
